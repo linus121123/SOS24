eca2f298920ca089d55a1bacb22040c162ef8b4d
linus121123 <linusgronroos@gmail.com>
2024-03-25 15:19:02 +0200

    .

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,7 +1,9 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,10 +12,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView outputText;
+    EditText inputText;
+
+    double[] values = { 3.5, 4.4, 9.0, 1.2, 6.5, 12.0 };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        outputText = findViewById(R.id.outText);
+        inputText = findViewById(R.id.inputText);
+
+        double version = 3.14159;
+        int year = 2024;
+        String author = "Linus";
+
+        outputText.setText(String.format(
+                "Appens version %.1f år %d by %s",
+                version,
+                year,
+                author)
+        );
+
+    }
+
+    public void buttonHandler(View view) {
+        String text = inputText.getText().toString();
+        outputText.setText(text);
     }
 }
